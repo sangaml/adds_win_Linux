d45797fa327fefe62a32a9055bc39af675d9eb78
Sangam <36110390+sangaml@users.noreply.github.com>
2018-10-26 10:44:45 +0530

    Create Linuxvm.ps1

---
+++ Linuxvm.ps1
@@ -0,0 +1,159 @@
+# Create variables to store the location and resource group names.
+$location = "centralus"
+$ResourceGroupName = "myResourceGroup"
+
+New-AzureRmResourceGroup `
+  -Name $ResourceGroupName `
+  -Location $location
+
+  # Create storage resources
+# Create variables to store the storage account name and the storage account SKU information
+$random = (New-Guid).ToString().Substring(0,8)
+$StorageAccountName = "mystorageaccount$random"
+$SkuName = "Standard_LRS"
+
+# Create a new storage account
+$StorageAccount = New-AzureRMStorageAccount `
+  -Location $location `
+  -ResourceGroupName $ResourceGroupName `
+  -Type $SkuName `
+  -Name $StorageAccountName
+
+Set-AzureRmCurrentStorageAccount `
+  -StorageAccountName $storageAccountName `
+  -ResourceGroupName $resourceGroupName
+
+# Create a storage container to store the virtual machine image
+$containerName = 'osdisks'
+$container = New-AzureStorageContainer `
+  -Name $containerName `
+  -Permission Blob
+
+  #Create networking resources
+# Create a subnet configuration
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name mySubnet$random `
+  -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -Name MyVnet$random `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $subnetConfig
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzureRmPublicIpAddress `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -AllocationMethod Static `
+  -IdleTimeoutInMinutes 4 `
+  -Name "mypublicdns$(Get-Random)"
+
+#Create a network security group and a network security group rule
+# Create an inbound network security group rule for port 3389
+$nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig `
+  -Name myNetworkSecurityGroupRuleRDP `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 3389 `
+  -Access Allow
+
+# Create an inbound network security group rule for port 80
+$nsgRuleWeb = New-AzureRmNetworkSecurityRuleConfig `
+  -Name myNetworkSecurityGroupRuleWWW `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 1001 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+
+# Create a network security group
+$nsg = New-AzureRmNetworkSecurityGroup `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -Name myNetworkSecurityGroup `
+  -SecurityRules $nsgRuleRDP,$nsgRuleWeb
+
+  #Create a network card for the virtual machine
+# Create a virtual network card and associate it with public IP address and NSG
+$nic = New-AzureRmNetworkInterface `
+  -Name myNic `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
+#Create a virtual machine
+
+# Define a credential object to store the username and password for the virtual machine
+$UserName='demouser'
+$Password='Password@123'| ConvertTo-SecureString -Force -AsPlainText
+$Credential=New-Object PSCredential($UserName,$Password)
+
+# Create the virtual machine configuration object
+$VmName = "LinuxVirtualMachine$random"
+$VmSize = "Standard_A1"
+$VirtualMachine = New-AzureRmVMConfig `
+  -VMName $VmName `
+  -VMSize $VmSize
+
+$VirtualMachine = Set-AzureRmVMOperatingSystem `
+  -VM $VirtualMachine `
+  -Windows `
+  -ComputerName "MainComputer" `
+  -Credential $Credential
+
+$VirtualMachine = Set-AzureRmVMSourceImage `
+  -VM $VirtualMachine `
+  -PublisherName "MicrosoftWindowsServer" `
+  -Offer "UbuntuServer" `
+  -Skus "16.04.2-LTS" `
+  -Version "latest"
+
+$osDiskName = "OsDisk"
+$osDiskUri = '{0}vhds/{1}-{2}.vhd' -f `
+  $StorageAccount.PrimaryEndpoints.Blob.ToString(),`
+  $vmName.ToLower(), `
+  $osDiskName
+
+# Sets the operating system disk properties on a virtual machine.
+$VirtualMachine = Set-AzureRmVMOSDisk `
+  -VM $VirtualMachine `
+  -Name $osDiskName `
+  -VhdUri $OsDiskUri `
+  -CreateOption FromImage | `
+  Add-AzureRmVMNetworkInterface -Id $nic.Id
+
+# Create the virtual machine.
+New-AzureRmVM `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -VM $VirtualMachine
+
+  #insatll Custom ScriptExtension
+
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName myResourceGroup `
+-VMName $VmName `
+-Location $location `
+-FileUri myURL `
+-Run 'Linuxext.ps1' `
+-Name ADScriptExtension
+
+
+#install Azure AD Connect
+$ad_msi = https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi
+
+Start-Process $ad_msi /qn
+
+
+$filename = "AzureADConnect.msi"
+$node_msi = "$PSScriptRoot\$filename"
