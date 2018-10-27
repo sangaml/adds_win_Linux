b4bb536b189a23b540eaabd2413d365760dd9a37
Sangam <36110390+sangaml@users.noreply.github.com>
2018-10-27 23:32:09 +0530

    Update azureADDS_integratewith_VMs.ps1

--- azureADDS_integratewith_VMs.ps1
+++ azureADDS_integratewith_VMs.ps1
@@ -152,17 +152,67 @@ New-AzureRmVM `
   -Location $location `
   -VM $winVirtualMachine
 
-  <#insatll Custom ScriptExtension
-
-Set-AzureRmVMCustomScriptExtension -ResourceGroupName $ResourceGroupName `
-    -VMName $winVmName `
-    -Location $location `
-    -FileUri myURL `
-    -Run 'windowesext.ps1' `
-    -Name ADScriptExtension
-    #>
     Write-Host "----- Launched Windows VM Successfully!!-----" -ForegroundColor Green
-########################################################################
+    Get-AzureRmVM -ResourceGroupName $RG | Where-Object {$_.Name -like 'WinVirtualMachine'} |
+    Add-JDAzureRMVMToDomain -DomainName sangamlonkar14.cf -Verbose
+    function Add-JDAzureRMVMToDomain {
+    
+    param(
+       [Parameter(Mandatory=$true)]
+       [string]$DomainName,
+       [Parameter(Mandatory=$false)]
+       [System.Management.Automation.PSCredential]$Credentials = (Get-Credential -Message 'Enter the domain join credentials'),
+       [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+       [Alias('VMName')]
+       [string]$Name,
+       [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+       [ValidateScript({Get-AzureRmResourceGroup -Name $_})]
+       [string]$ResourceGroupName
+    )
+       begin {
+           #Define domain join settings (username/domain/password)
+           $Settings = @{
+               Name = $DomainName
+               User = $Credentials.UserName
+               Restart = "true"
+               Options = 3
+           }
+           $ProtectedSettings =  @{
+                   Password = $Credentials.GetNetworkCredential().Password
+           }
+           Write-Verbose -Message "Domainname is: $DomainName"
+       }
+       process {
+           try {
+               $RG = Get-AzureRmResourceGroup -Name $ResourceGroupName
+               $JoinDomainHt = @{
+                   ResourceGroupName = $RG.ResourceGroupName
+                   ExtensionType = 'JsonADDomainExtension'
+                   Name = 'joindomain'
+                   Publisher = 'Microsoft.Compute'
+                   TypeHandlerVersion = '1.0'
+                   Settings = $Settings
+                   VMName = $Name
+                   ProtectedSettings = $ProtectedSettings
+                   Location = $RG.Location
+               }
+               Write-Verbose -Message "Joining $Name to $DomainName"
+               Set-AzureRMVMExtension @JoinDomainHt
+               Start-Sleep 360
+    
+             Set-AzureRmVMCustomScriptExtension -ResourceGroupName $RG `
+                   -VMName $Name -Name "myCustomScript" `
+                   -FileUri "https://raw.githubusercontent.com/sangaml/adds_win_Linux/master/userpermission.ps1" `
+                   -Run "userpermission.ps1" `
+                   -Location "west us"
+           } catch {
+               Write-Warning $_
+           }
+       }
+       end { }
+    }
+    
+    ########################################################################
 #                                                                      #
 #          Launching Linux VM                                          #
 #                                                                      #
@@ -269,12 +319,13 @@ New-AzureRmVM `
  -Location $location `
   -VM $VirtualMachine
 
-<#insatll Custom ScriptExtension
-Set-AzureRmVMCustomScriptExtension -ResourceGroupName $ResourceGroupName `
--VMName $winVmName `
--Location $location `
--FileUri myURL `
--Run 'windowesext.ps1' `
--Name ADScriptExtension
-#>
 Write-Host "----- Launched Linux VM Successfully!!-----" -ForegroundColor Green
+Write-Host "----- Launched Linux VM Successfully!!-----" -ForegroundColor Green
+Write-Host "----- Adding Extenstion Now -----" -ForegroundColor Green
+$TheURI = "https://raw.githubusercontent.com/sangaml/adds_win_Linux/master/joinlinux.sh"
+$Settings = @{"fileUris" = @($TheURI); "commandToExecute" = "./joinlinux.sh"};
+#
+Set-AzureRmVMExtension -ResourceGroupName $ResourceGroupName -Location $location -VMName $VmName `
+-Name "customScript" `
+-Publisher "Microsoft.Azure.Extensions" -Type "customScript" -TypeHandlerVersion "2.0" `
+-Settings $Settings
